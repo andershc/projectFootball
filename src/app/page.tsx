458a57847d09d1d71c5d7c22028aafbec90246ef
andersheftoycarlsen <anders.heftoy@gmail.com>
2023-04-28 16:15:45 +0200

    Refactor to make code more readable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,17 @@ import styles from "../styles/Home.module.css";
 import { useState, useEffect } from "react";
 import { useAuthContext } from "../../lib/AuthContext";
 import { useGuessContext } from "../../lib/GuessContext";
-import GuessContainer from "../components/guessContainer/GuessContainer";
-import PlayerInput from "../components/playerInput/PlayerInput";
 import { Player, DailyPlayer, Team, TransferData } from "../types";
 import { getPlayers, getDailyPlayer, getRandomPlayer } from "./api/fetchPlayers";
 import Loading from "./loading";
-import Image from "next/image";
-import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 import Button from "../components/button/Button";
 import { useRouter, useSearchParams } from "next/navigation";
 import { usePlayersContext } from "../../lib/PlayersContext";
 import { updateScore } from "./api/updateData";
 import Lottie from 'react-lottie';
 import confetti from '../../public/static/lotti/confetti.json'
-import WhatshotIcon from '@mui/icons-material/Whatshot';
 import { getUserHistory } from "./api/fetchUserData";
+import CareerPathGame from "../components/careerpathGame";
 
 interface Transfer extends Team {
   type: string;
@@ -45,7 +41,6 @@ export default function Home() {
   const { 
     correctPlayer,
     setCorrectPlayer,
-    transferData,
     setTransferData,
   } = useGuessContext();
 
@@ -97,7 +92,7 @@ export default function Home() {
   }, [setCorrectPlayer, setTransferData, user, router, players.length, setPlayers, fetchDate]);
 
   const guessLimit = clubs?.length || 0;
-  console.log(guessedPlayers);
+
   const handlePlayerSelect = (player: Player) => {
     setGuessedPlayers((prev) => [...prev, player]);
     const playersCopy = [...guessedPlayers, player];
@@ -117,6 +112,17 @@ export default function Home() {
     }
   };
 
+  const handleNewPlayer = async () => {
+    const newPlayerData = await getRandomPlayer(players);
+    if (newPlayerData === undefined) return;
+    setIsDailyPlayer(false);
+    setCompleted(null);
+    setGuessedPlayers([]);
+    setCorrectPlayer(newPlayerData.player);
+    setTransferData(newPlayerData.transferData);
+    setClubs(getTransferClubs(newPlayerData.transferData, newPlayerData.player.team)); // Add this line to update the clubs
+  };
+
   const defaultOptions = {
     loop: false,
     autoplay: true,
@@ -129,119 +135,20 @@ export default function Home() {
   return (
     <div className={styles.mainContainer}>
       {loading ? <Loading /> :
-      <div className={styles.mainContent}>
-        <h1 className={styles.title}>Guess the Player</h1>
-        <h2>{guessedPlayers.length} / {guessLimit}</h2>
-        <p className={styles.comeback}>{completed && "Come back tomorrow for a new challenge!"}</p>
-        { completed || guessedPlayers.length === guessLimit ?
-            <div className={styles.completedRow}>
-              {completed ? 
-                (
-                  <h2>Congratulations! 🏆</h2>
-                ):(
-                  <>
-                    <h2>Tough luck...😞</h2>
-                    <p>The correct player was:</p>
-                  </>
-                )
-              }
-
-              <Image
-                className={styles.playerPhoto}
-                src={correctPlayer.photo}
-                alt="player"
-                width={60}
-                height={60}
-              />
-              <p className={styles.playerName}>{correctPlayer.name}</p>
-            </div> : null
-          }
-        {
-            transferData && transferData.length > 0 &&
-            <div className={styles.transfersContainer}>
-                <div className={styles.clubs}>
-                  {clubs.map((data) => (
-                    <div key={clubs.indexOf(data)} className={styles.transfer}>
-                      {clubs.indexOf(data) !== 0 && <DoubleArrowIcon className={styles.arrow}/>}
-                      <div className={styles.club}>
-                        
-                        {clubs.indexOf(data) <= guessedPlayers.length || completed ?
-                        (
-                          <>
-                            <p>{data.type == 'Loan' && data.type}</p>
-                            <Image
-                              src={data.logo}
-                              alt={data.name}
-                              width={48}
-                              height={48}
-                            />
-                          </>
-                          
-                        ):(
-                          <Image
-                            src={'https://hatscripts.github.io/circle-flags/flags/xx.svg'}
-                            alt="flag"
-                            width={32}
-                            height={32}
-                          />
-                        )}
-                        
-                        <p>{data.year}</p>
-                      </div>
-                    </div>
-                ))}
-                </div>
-            </div>
-          }
-        {completed === null && <PlayerInput players={players} onSelect={handlePlayerSelect}/>}
-        { guessedPlayers.length > 0 &&
-          <div className={styles.guesses}>
-            <p>Guessed players:</p>
-            {guessedPlayers.map((guessedPlayer) => (
-              <GuessContainer
-                key={guessedPlayer.id} 
-                player={guessedPlayer}
-                correct={guessedPlayer.id === correctPlayer.id}
-                index={guessedPlayers.indexOf(guessedPlayer) + 1}
-              />
-            ))}
-          </div>
-        }
-        {(stats.totalCorrect === undefined || stats.totalCorrect === 0) && completed !== null ? 
-        null : 
-        <div className={styles.stats}>
-          <WhatshotIcon
-            className={styles.statsIcon}
-          />
-          <p>{stats.totalCorrect !== undefined && stats.totalCorrect !== 0 ? 
-          (stats.totalAttempts > 1 ?
-            (
-              `${stats.totalCorrect} people have guessed the correct player today`
-            ) : (
-              `${stats.totalCorrect} person has guessed the correct player today`
-            )
-          ) : (
-            "You can be the first to guess the correct player!"
-          )
-          }
-          </p>
-        </div>}
-        
-        
-    </div>
+        <CareerPathGame
+          guessedPlayers={guessedPlayers}
+          correctPlayer={correctPlayer}
+          clubs={clubs}
+          completed={completed}
+          stats={stats}
+          handlePlayerSelect={handlePlayerSelect}
+          players={players}
+        />
       }
       { process.env.NODE_ENV === 'development' &&
         <div className={styles.gamesContainer}>
         <Button
-          onClick={() => getRandomPlayer(players).then((player) => {
-            if(player === undefined) return;
-            setIsDailyPlayer(false);
-            setCompleted(false);
-            setGuessedPlayers([])
-            setCorrectPlayer(player?.player)
-            setTransferData(player?.transferData)
-            }
-          )}
+          onClick={() => handleNewPlayer()}
           text="Get new player"
           className={styles.newPlayerButton}
         />
