c3da32ee997c9040f4f043fac81eda90108537cc
Anders Heftøy Carlsen <77790244+andershc@users.noreply.github.com>
2023-09-21 20:46:44 +0200

    New suspense (#10)
    * Added new loading skeleton
    
    * Styling to the loader
    
    * A fix I think
    
    * Fixed the fetching of players from local storage
    
    * Pulse loading

--- src/app/api/updateData.ts
+++ src/app/api/updateData.ts
@@ -7,51 +7,103 @@ import {
 import { type UserType } from "../../../lib/AuthContext";
 import { type GuessResult, type Player } from "../../types";
 
+/**
+ * Calculate the score for the current game
+ * @param completed - Whether the game was completed
+ * @param guessLimit - The number of guesses allowed
+ * @param guesses - The guesses made by the user
+ * @returns The score for the current game
+ * */
+const calculateScore = (
+  completed: boolean | null,
+  guessLimit: number,
+  guesses: Player[]
+): number => {
+  return completed ?? false
+    ? Math.round(((guessLimit - guesses.length + 1) / guessLimit) * 10)
+    : 0;
+};
+
+/**
+ * Update the local storage with the current game data
+ * @param date - The date of the current game
+ * @param guesses - The guesses made by the user
+ * @param score - The score for the current game
+ * @param completed - Whether the game was completed
+ * @returns void
+ * */
+const updateLocalStorage = (
+  date: string,
+  guesses: Player[],
+  score: number,
+  completed: boolean | null
+): void => {
+  const gameData: GuessResult = {
+    guessedPlayers: guesses,
+    guesses: guesses.length,
+    points: score,
+    completed,
+  };
+  localStorage.setItem(date, JSON.stringify(gameData));
+};
+
+/**
+ * Update the user's score
+ * @param currentUser - The current user
+ * @param guesses - The guesses made by the user
+ * @param guessLimit - The number of guesses allowed
+ * @param completed - Whether the game was completed
+ * @param date - The date of the current game
+ * @returns void
+ * */
 export async function updateScore(
   currentUser: UserType | null | undefined,
   guesses: Player[],
   guessLimit: number,
-  completed: boolean | null
+  completed: boolean | null,
+  date: string | null = null
 ): Promise<void> {
-  // Calculate score
-  const score =
-    completed === true
-      ? Math.round(((guessLimit - guesses.length + 1) / guessLimit) * 10)
-      : 0;
-  if (completed !== null) {
-    await updateDailyPlayerStats(completed)
-      .then(() => {
-        console.log("Daily player stats updated successfully");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  // Set default date if not provided
+  if (date == null) {
+    date = moment().tz("America/New_York").format("YYYY-MM-DD");
   }
-  if (currentUser === null || currentUser === undefined) {
+
+  const score = calculateScore(completed, guessLimit, guesses);
+
+  // Update Daily Player Stats
+  if (completed !== null) {
+    try {
+      await updateDailyPlayerStats(completed);
+      console.log("Daily player stats updated successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Check if user is logged in
+  if (currentUser == null) {
     console.log("User is not logged in");
-    const date = moment().tz("America/New_York");
-    const formatCurrentDate = `${date.year()}-${date.date()}-${
-      date.month() + 1
-    }`;
-    const gameData: GuessResult = {
-      guessedPlayers: guesses,
-      guesses: guesses.length,
-      points: score,
-      completed,
-    };
-    localStorage.setItem(formatCurrentDate, JSON.stringify(gameData));
-  } else {
-    console.log("User is logged in");
-    await updateDailyScore(currentUser, score, completed, guesses)
-      .then(() => {
-        console.log("Score updated successfully");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    updateLocalStorage(date, guesses, score, completed);
+    return;
+  }
+
+  console.log("User is logged in");
+
+  // Update Daily Score
+  try {
+    await updateDailyScore(currentUser, score, completed, guesses);
+    console.log("Score updated successfully");
+  } catch (error) {
+    console.log(error);
   }
 }
 
+/**
+ * Update the user's username
+ * @param currentUser - The current user
+ * @param username - The user's username
+ * @returns void
+ * */
 export async function updateUsername(
   currentUser: UserType | null,
   username: string
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,48 @@
-import Image from "next/image";
-import LoadingSpinner from "../../public/static/images/tail-spin.svg";
+"use client";
+
+import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
+import styles from "./loading.module.css";
 
 export default function Loading(): JSX.Element {
   return (
-    <>
-      <Image src={LoadingSpinner} alt={"/"} width={50} height={50} priority />
-      <div>Loading...</div>
-    </>
+    <div className={styles.mainContainer}>
+      <div className={styles.mainContent}>
+        <div>
+          <h1 className={styles.title}>Guess the Player</h1>
+        </div>
+        <h2></h2>
+        <div className={styles.transfersContainer}>
+          <div className={styles.circleWithArrowLoader}>
+            <div className={styles.clubs}>
+              <div className={styles.club}>
+                <div className={styles.circle}></div>
+                <div className={styles.year}></div>
+              </div>
+            </div>
+            <CircleWithArrowLoader />
+            <CircleWithArrowLoader />
+            <CircleWithArrowLoader />
+          </div>
+        </div>
+        <div className={styles.inputContainerLoader}>
+          <div className={styles.inputloader} />
+        </div>
+
+        <h3>Want to play more?</h3>
+        <div className={styles.buttonLoader}></div>
+      </div>
+    </div>
+  );
+}
+
+function CircleWithArrowLoader(): JSX.Element {
+  return (
+    <div className={styles.circleWithArrowLoader}>
+      <DoubleArrowIcon className={styles.arrow} />
+      <div className={styles.club}>
+        <div className={styles.circle}></div>
+        <div className={styles.year}></div>
+      </div>
+    </div>
   );
 }
